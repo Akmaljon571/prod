f15a3d84e60d5c89261bcaf246d81302cb4bccf8
Akmaljon571 <akmaljon079@gmail.com>
2023-10-17 22:56:40 +0300

    add: registr-kirish

--- src/components/auth/registr-kirish.jsx
+++ src/components/auth/registr-kirish.jsx
@@ -2,9 +2,10 @@ import { Link } from 'react-router-dom';
 import logo from '../../img/LinCor.svg';
 import { Button } from '@mui/material';
 import './auth.scss';
+import { useState } from 'react';
 
 function RegistrFinish() {
-  
+  const [img, setImg] = useState('');
 
   return (
     <div className="auth finish">
@@ -13,6 +14,15 @@ function RegistrFinish() {
       </Link>
       <h2>Profil</h2>
       <p>Ma’lumotingizni kiriting.</p>
+      <div>
+        <img src={img || 'https://cdn-icons-png.flaticon.com/512/3135/3135715.png'} alt="" />
+        <p>JPG yoki PNG formatdagi
+            sur’atingizni joylang.</p>
+        <label htmlFor='a'>
+          <span>Rasm joylash</span>
+          <input onChange={(e) => setImg(URL.createObjectURL(e.target.files[0]))} id='a' type="file" style={{display: "none"}} /> 
+        </label>
+      </div>
       <label>
         <span>Ismingiz</span>
         <input type="text" />
