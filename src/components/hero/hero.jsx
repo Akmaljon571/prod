b7ab7a1286b08edcf3858cb318ffc75d5d153b47
Akmaljon571 <akmaljon079@gmail.com>
2023-10-23 03:01:54 +0300

    finish: hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,20 +1,31 @@
 import './hero.scss';
 import { Button } from '@mui/material';
-import player from '../../img/video-player.svg';
+import player from '../../img/Vector 6.svg';
 import girl from '../../img/hero-girl.png';
+import img1 from '../../img/hero1.png';
+import img2 from '../../img/hero2.png';
+import img3 from '../../img/hero3.png';
 
 function Hero() {
   return (
     <div className="hero">
       <div className="left">
-        <h1>online Til o‘rgan Koreada o‘qi!</h1>
-        <p>Bitta siteni o‘zida bilim oling testdan o‘ting koreaga boring!</p>
-        <Button variant="text">
-          birinchi dars
+        <h1>
+          Online til o‘rganing Koreada <span>O’qish</span> Kutmoqda
+        </h1>
+        <Button variant="text">birinchi dars</Button>
+        <Button className="img" variant="contained">
           <img src={player} alt="Video player" />
         </Button>
       </div>
-      <img className="right" src={girl} alt="Hero Girl" />
+      <div className="image">
+        <div className="image-fath">
+          <img className="img1" src={img1} alt="Rasm1" />
+          <img className="img2" src={img2} alt="Rasm2" />
+          <img className="img3" src={img3} alt="Rasm3" />
+          <img className="girl" src={girl} alt="Hero Girl" />
+        </div>
+      </div>
     </div>
   );
 }
